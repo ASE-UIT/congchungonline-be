ca4ba469bf94b2211c26bf0b99f8f2f691536e03
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-29 17:51:54 +0700

    chore: Update roles configuration and authentication middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,19 @@
 const passport = require('passport');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
-const { Role } = require('../models');
+const { getPermissionsByRoleName } = require('../config/roles');
 
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
   if (err || info || !user) {
     return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
   }
   req.user = user;
-  // console.log('User:', user);
 
   if (requiredRights.length) {
     try {
-      console.log('User role:', user.role);
+      const userPermissions = await getPermissionsByRoleName(user.role);
 
-      const role = await Role.findOne({ name: user.role });
-
-      console.log('Fetched role:', role);
-
-      if (!role) {
-        console.error('Role not found:', user.role);
-        return reject(new ApiError(httpStatus.FORBIDDEN, 'Role not found'));
-      }
-
-      const userRights = role.permissions;
-      const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
+      const hasRequiredRights = requiredRights.every((requiredRight) => userPermissions.includes(requiredRight));
       if (!hasRequiredRights && req.params.userId !== user.id) {
         return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
       }
