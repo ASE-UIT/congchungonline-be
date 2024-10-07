2e1d8b48c7340c1394c228e807318c009b74d428
DQP_UIT <22521149@gm.uit.edu.vn>
2024-10-08 00:52:35 +0700

    first commit for models UT

---
+++ tests/unit/models/role.model.test.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const faker = require('faker');
+const { Role } = require('../../../src/models');
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
+
+describe('Role model', () => {
+    describe('Role validation', () => {
+        let newRole;
+        beforeEach(() => {
+            newRole = {
+                name: faker.name.jobType(),
+                permissions: [faker.random.word(), faker.random.word()], 
+            };
+        });
+
+        test('should correctly validate a valid role', async () => {
+            await expect(new Role(newRole).validate()).resolves.toBeUndefined();
+        });
+
+        test('should throw a validation error if name is missing', async () => {
+            newRole.name = undefined;
+            await expect(new Role(newRole).validate()).rejects.toThrow();
+        });
+
+        test('should throw a validation error if permissions are missing', async () => {
+            newRole.permissions = undefined;
+            await expect(new Role(newRole).validate()).rejects.toThrow();
+        });
+
+        test('should throw a validation error if permissions are not an array', async () => {
+            newRole.permissions = 'notAnArray';
+            await expect(new Role(newRole).validate()).rejects.toThrow();
+        });
+    });
+
+    describe('Role statics', () => {
+        test('should return false if role name is not taken', async () => {
+            await expect(Role.isRoleNameTaken(newRole.name)).resolves.toBe(false);
+        });
+
+        test('should return true if role name is already taken', async () => {
+            await new Role(newRole).save();
+            await expect(Role.isRoleNameTaken(newRole.name)).resolves.toBe(true);
+        });
+
+        test('should return false if role name is taken but excluded role id is provided', async () => {
+            const role = await new Role(newRole).save();
+            await expect(Role.isRoleNameTaken(newRole.name, role._id)).resolves.toBe(false);
+        });
+    });
+});
---
+++ tests/unit/models/token.model.test.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const faker = require('faker');
+const { Token } = require('../../../src/models');
+const { tokenTypes } = require('../../../src/config/tokens');
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
+
+describe('Token model', () => {
+    describe('Token validation', () => {
+        let newToken;
+        beforeEach(() => {
+            newToken = {
+                token: faker.random.alphaNumeric(32), 
+                user: new mongoose.Types.ObjectId(),
+                type: tokenTypes.REFRESH,
+                expires: new Date(Date.now() + 60 * 60 * 1000), 
+            };
+        });
+
+        test('should correctly validate a valid token', async () => {
+            await expect(new Token(newToken).validate()).resolves.toBeUndefined();
+        });
+
+        test('should throw a validation error if token is missing', async () => {
+            newToken.token = undefined;
+            await expect(new Token(newToken).validate()).rejects.toThrow();
+        });
+
+        test('should throw a validation error if user is missing', async () => {
+            newToken.user = undefined;
+            await expect(new Token(newToken).validate()).rejects.toThrow();
+        });
+
+        test('should throw a validation error if type is invalid', async () => {
+            newToken.type = 'invalidType';
+            await expect(new Token(newToken).validate()).rejects.toThrow();
+        });
+
+        test('should throw a validation error if expires is missing', async () => {
+            newToken.expires = undefined;
+            await expect(new Token(newToken).validate()).rejects.toThrow();
+        });
+    });
+
+    describe('Token toJSON()', () => {
+        test('should not return token when toJSON is called', () => {
+            const newToken = {
+                token: faker.random.alphaNumeric(32), 
+                user: new mongoose.Types.ObjectId(), 
+                type: tokenTypes.REFRESH, 
+                expires: new Date(Date.now() + 60 * 60 * 1000), 
+            };
+            expect(new Token(newToken).toJSON()).not.toHaveProperty('token'); // this case went wrong
+        });
+    });
+});
