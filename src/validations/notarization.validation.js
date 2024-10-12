f2911ed96eaf283311e15b0556146c1bdedec46a
HGB3009 <baobaokg9@gmail.com>
2024-10-12 17:36:21 +0700

    Refactor: update document model with notarizationRoute

--- src/validations/notarization.validation.js
+++ src/validations/notarization.validation.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
+const { objectId } = require('./custom.validation');
 
 const createDocument = {
   body: Joi.object().keys({
     files: Joi.object().keys({}),
-    notaryService: Joi.string().required(),
-    notaryField: Joi.string().required(),
+    notarizationServiceId: Joi.string().custom(objectId),
+    notarizationFieldId: Joi.string().custom(objectId),
     requesterInfo: Joi.string().required(),
   }),
 };
